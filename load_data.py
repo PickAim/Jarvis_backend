c34d603d0b383471cc09a537e59427eff1d5b603
Pelidyai <dima-pile@mail.ru>
2022-08-23 00:43:20 +0700

    Cost stats calculation + some refactoring features

--- load_data.py
+++ load_data.py
@@ -1,14 +1,13 @@
 import requests
 import sys
 import re
+import constants
 
 from os import listdir
 from os.path import isfile, join
 from datetime import datetime, timedelta
 from calc import get_frequency_stats
-from jarvis_utils import create_parser
-
-data_path = "data"
+from jarvis_utils import create_parser, load_data
 
 
 def get_all_product_niche(text: str):
@@ -42,22 +41,11 @@ def get_all_product_niche(text: str):
                 sum += obj['price']['RUB']
                 count += 1
         avr_mass.append(sum / count)
-    with open(join(data_path, text + ".txt"), 'w+', encoding='utf-8') as f:
+    with open(join(constants.data_path, text + ".txt"), 'w+', encoding='utf-8') as f:
         for i in range(len(avr_mass)):
             if i % 10 == 0:
                 f.write("\n")
             f.write(str(avr_mass[i]) + ",")
-
-
-def load_data(filename: str) -> list[float]:
-    result = []
-    with (open(filename, "r")) as file:
-        lines = file.readlines()
-        for line in lines:
-            for cost in line.split(","):
-                if cost != "" and cost != "\n":
-                    result.append(float(cost))
-    return result
 
 
 if __name__ == '__main__':
@@ -65,16 +53,13 @@ if __name__ == '__main__':
     namespace = parser.parse_args(sys.argv[1:])
     text_to_search = namespace.text.lower()
     text_to_search = re.sub(' +', ' ', text_to_search)
-    only_files = [f.split('.')[0] for f in listdir(data_path) if isfile(join(data_path, f))]
+    only_files = [f.split('.')[0] for f in listdir(constants.data_path) if isfile(join(constants.data_path, f))]
     if not only_files.__contains__(text_to_search):
         get_all_product_niche(text_to_search)
-    filename = str(join(data_path, text_to_search + ".txt"))
+    filename = str(join(constants.data_path, text_to_search + ".txt"))
     cost_data = load_data(filename)
     n_samples = int(len(cost_data) * 0.1)  # todo think about number of samples
     x, y = get_frequency_stats(cost_data, n_samples)
-    with (open("out.txt", "w")) as file:
+    with (open(join(constants.out_path, "out.txt"), "w")) as file:
         for i in range(len(x)):
             file.write(f'{x[i]}, {y[i]}\n')
-
-
-
