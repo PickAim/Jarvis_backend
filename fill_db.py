77dedac2746fae2ff002d2fd3c2eebb806bf873e
Sawyron <s4pakvv@gmail.com>
2022-08-31 01:30:45 +0700

    implemented product and product price history db filling

--- fill_db.py
+++ fill_db.py
@@ -1,6 +1,7 @@
+import asyncio
 from domain.wildberies_data_provider import WildBerriesDataProvider
 from database.db_config import SessionLocal
-from database.tables import Category, Niche
+from database.tables import Category, Niche, Product, ProductCostHistory
 from sqlalchemy.orm import sessionmaker
 from create_tables import create_tables
 
@@ -39,6 +40,41 @@ class DbFiller():
                         )
                 session.add_all(db_niches)
 
+    def fill_niche_products(self, niche: str, pages: int = 1):
+        with self.__sessionmaker() as session:
+            with session.begin():
+                db_products = []
+                niche_id = session.query(Niche).filter(
+                    Niche.name == niche).first().id
+                products = self.__api.get_products_by_niche(niche, pages)
+                for product in products:
+                    db_products.append(Product(
+                        id=product[1],
+                        name=product[0],
+                        niche_id=niche_id
+                    ))
+                session.add_all(db_products)
+
+    def fill_niche_price_history(self, niche: str):
+        with self.__sessionmaker() as session:
+            with session.begin():
+                db_histories = []
+                niche_id = session.query(Niche).filter(
+                    Niche.name == niche).first().id
+                products: list[Product] = session.query(
+                    Product).filter(Product.niche_id == niche_id).all()
+                for product in products:
+                    histories = self.__api.get_product_price_history(
+                        product.id)
+                    for history in histories:
+                        db_histories.append(
+                            ProductCostHistory(
+                                cost=history[0],
+                                date=history[1],
+                                product_id=product.id
+                            ))
+                session.add_all(db_histories)
+
 
 if __name__ == '__main__':
     key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NJRCI6IjZkNDVmMmRjLTQ5ODEtNDFlOS1hMzRkLTlhNDA5YmY2MGZiMSJ9.1VoUp9Od9dzSWSNVSQjQnRujUvqOUY4oxO-pZXAqI1Q'
@@ -47,3 +83,5 @@ if __name__ == '__main__':
     create_tables()
     filler.fill_categories()
     filler.fill_niches()
+    filler.fill_niche_products('Автобаферы')
+    filler.fill_niche_price_history('Автобаферы')
