092ecbd15e9e14feae51f6b1c8bf65e2c763e3c7
Pelidyai <dima-pile@mail.ru>
2022-09-11 18:44:18 +0700

    Some renaming

--- domain/get_niche_json.py
+++ domain/get_niche_json.py
@@ -14,31 +14,30 @@ def get_parent():
                                                 '.eyJhY2Nlc3NJRCI6IjZkNDVmMmRjLTQ5ODEtNDFlOS1hMzRkLTlhNDA5YmY2MGZiMSJ9.1VoUp9Od9dzSWSNVSQjQnRujUvqOUY4oxO-pZXAqI1Q'})
     json_code = response.json()
     category = []
-    for i in json_code['data']:
-        category.append(i)
+    for data in json_code['data']:
+        category.append(data)
     return category
 
 
 def get_niche():
-    category = get_parent()
+    categories = get_parent()
     dict_niche_by_category = dict()
     session = requests.Session()
     adapter = requests.adapters.HTTPAdapter(
         pool_connections=100,
         pool_maxsize=100)
     session.mount('http://', adapter)
-    for i in category:
-
-        response = session.get('https://suppliers-api.wildberries.ru/api/v1/config/object/byparent?parent=' + i,
+    for category in categories:
+        response = session.get('https://suppliers-api.wildberries.ru/api/v1/config/object/byparent?parent=' + category,
                                headers={
                                    'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'
                                                     '.eyJhY2Nlc3NJRCI6IjZkNDVmMmRjLTQ5ODEtNDFlOS1hMzRkLTlhNDA5YmY2MGZiMSJ9.1VoUp9Od9dzSWSNVSQjQnRujUvqOUY4oxO-pZXAqI1Q'})
         json_code = response.json()
         niche = []
-        for j in json_code['data']:
-            niche.append(j['name'])
+        for data in json_code['data']:
+            niche.append(data['name'])
         niche.sort()
-        dict_niche_by_category[i] = niche
+        dict_niche_by_category[category] = niche
     with open("data_file.json", "w", encoding='utf-8') as write_file:
         json.dump(dict_niche_by_category, write_file, ensure_ascii=False)
 
