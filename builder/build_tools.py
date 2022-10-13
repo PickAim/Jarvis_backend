e4d53820645bad9f84058f3691724effa4ce5a02
Pelidyai <dima-pile@mail.ru>
2022-10-13 20:14:08 +0700

    CI/CD improve: build bugs fixes, fix publishing

--- builder/build_tools.py
+++ builder/build_tools.py
@@ -1,7 +1,8 @@
 import os
+from subprocess import run, PIPE, STDOUT
 from datetime import datetime
 
-from build_constants import _separator, element_name
+from build_constants import _separator, element_name, component_dir
 from os import system as sys
 
 
@@ -21,9 +22,18 @@ def get_my_version() -> str:
         return version
 
 
+def get_my_name(url: str) -> str:
+    return url.split('/')[-1].split('.')[0]
+
+
 def publish_to_git():
     version = get_my_version()
-    sys(f'git checkout -b release/{version}')
+    sys(f'git stash')
+    out = run(f'git switch -c release/{version}',
+              stdout=PIPE, stderr=STDOUT, universal_newlines=True, shell=True).stdout
+    if out.__contains__('fatal'):
+        sys(f'git checkout release/{version}')
+    sys(f'git stash pop')
     sys('git add ..')
     sys('git add .')
     sys(f'git commit -m \"[Auto: {datetime.now()}] publish {version}\"')
@@ -32,7 +42,12 @@ def publish_to_git():
 
 def publish_to_git_with_comment(comment):
     version = get_my_version()
-    sys(f'git checkout -b release/{version}')
+    sys(f'git stash')
+    out = run(f'git switch -c release/{version}',
+              stdout=PIPE, stderr=STDOUT, universal_newlines=True, shell=True).stdout
+    if out.__contains__('fatal'):
+        sys(f'git checkout release/{version}')
+    sys(f'git stash pop')
     sys('git add ..')
     sys('git add .')
     sys(f'git commit -m \"{comment}\"')
@@ -41,17 +56,32 @@ def publish_to_git_with_comment(comment):
 
 def build():
     with(open(__file__.replace(os.path.basename(__file__), '') + 'build.properties', 'r') as properties,
-         open(__file__.replace(os.path.basename(__file__), '') + 'dependencies.properties', 'r') as dependencies):
-        sys('rd /s /q jarvis_db')
+         open(__file__.replace(os.path.basename(__file__), '') + 'dependency.properties', 'r') as dependencies):
+        my_dir = __file__.replace(os.path.basename(__file__), '').replace('builder', '')
+        print(f'Installing requirements for {my_dir}')
+        run(f'pip install -r {os.path.join(my_dir, "requirements.txt")}',
+            stdout=PIPE, stderr=STDOUT, universal_newlines=True, shell=True)
+        sys(f'rd /s /q {component_dir}')
+        sys(f'mkdir {component_dir}')
         props = properties.readlines()
-        props_dict = {prop.split(_separator)[0]: prop.split(_separator)[1].replace('\n', '') for prop in props}
+        props_dict = {prop.split(_separator)[0]: prop.split(_separator)[1].replace('\n', '')
+                      for prop in props}
         depends = dependencies.readlines()
         depends_dict = {dependency.split(_separator)[0]: dependency.split(_separator)[1].replace('\n', '')
                         for dependency in depends}
         for prop_name in props_dict:
             if depends_dict.__contains__(prop_name):
-                sys(f'git clone --branch release/{props_dict[prop_name]} {depends_dict[prop_name]}')
-        sys('rd /s /q jarvis_db\\.git')
-        sys('rd /s /q jarvis_db\\builder')
-        sys('del /q jarvis_db\\.gitignore')
-        sys('del /q jarvis_db\\README.md')
+                dir_name = get_my_name(depends_dict[prop_name])
+                sys(f'mkdir {os.path.join(component_dir, dir_name)}')
+                sys(f'git clone --branch release/{props_dict[prop_name]} {depends_dict[prop_name]} '
+                    f'{os.path.join(component_dir, dir_name)}')
+                sys(f'rd /s /q {os.path.join(component_dir, dir_name)}\\.git')
+                sys(f'del /q {os.path.join(component_dir, dir_name)}\\.gitignore')
+                sys(f'del /q {os.path.join(component_dir, dir_name)}\\README.md')
+                sys(f'python {os.path.join(component_dir, dir_name, "builder", "build.py")}')
+
+                sys(f'rd /s /q {os.path.join(component_dir, dir_name)}\\builder')
+
+
+if __name__ == '__main__':
+    build()
