319762d6aac0180b8698cb062fbc2b28feba7af3
Pelidyai <dima-pile@mail.ru>
2023-01-25 21:13:03 +0700

    Remove unnecessary methods, requests and items

--- app.py
+++ app.py
@@ -1,32 +1,23 @@
 import uvicorn
-import re
-import numpy as np
 
 from fastapi import FastAPI
 
-from os.path import join
-from os.path import abspath, dirname
-
-# from jarvis_calc.database_interactors.db_access import DBUpdateProvider
-from jarvis_calc.utils.calc_utils import get_frequency_stats, get_frequency_stats_with_jorm
+from jarvis_calc.database_interactors.db_access import DBUpdateProvider
+from jarvis_calc.utils.calc_utils import get_frequency_stats_with_jorm
 from jarvis_calc.factories import JORMFactory
-from jarvis_calc.utils.margin_calc import unit_economy_calc, unit_economy_calc_with_jorm
+from jarvis_calc.utils.margin_calc import unit_economy_calc_with_jorm
 
 from jorm.market.infrastructure import Niche, Warehouse
+from jorm.market.service import Request
 
-from jdu.request.loader_utils import load_cost_data_from_file, load_niche_info
-# from jorm.market.service import Request
-
-from margin_item import MarginItem, MarginJormItem
+from margin_item import MarginJormItem
 
 
 app = FastAPI()
 
-storage_dir = join(dirname(__file__), "data")
-
 jorm_factory: JORMFactory = JORMFactory()  # TODO move to login or session creating request
 
-# db_updater: DBUpdateProvider = CalcUpdater()  # TODO move to login or session creating request
+db_updater: DBUpdateProvider  # TODO move to login or session creating request
 
 
 @app.post('/jorm_margin/')
@@ -34,8 +25,8 @@ def calc_margin(margin_item: MarginJormItem):
     niche: Niche = jorm_factory.niche(margin_item.niche)
     warehouse: Warehouse = jorm_factory.warehouse(margin_item.warehouse_name)
     result_dict = unit_economy_calc_with_jorm(margin_item.buy, margin_item.pack, niche,
-                                              warehouse, jorm_factory.get_current_client(),
-                                              margin_item.transit_price, margin_item.transit_count)
+                                              warehouse, jorm_factory.get_current_client(), margin_item.transit_price,
+                                              margin_item.transit_count, margin_item.transit_price)
     return result_dict
 
 
@@ -46,40 +37,10 @@ def upload_data(niche: str):
     return {'x': x, 'y': y}
 
 
-# @app.get('/save_request/{request}')
-# def upload_data(request_json: str):
-#     request_to_save: Request = jorm_factory.request(request_json)
-#     db_updater.save_request(request_to_save)
-
-
-# TODO delete all requests below
-@app.post('/margin/')
-def calc_margin(margin_item: MarginItem):
-    niche = margin_item.niche.lower()
-    niche = re.sub(' +', ' ', niche)
-    load_niche_info(niche, storage_dir, pages_num=1)
-    filename = abspath(str(join(storage_dir, niche + ".txt")))
-    costs = np.array(load_cost_data_from_file(filename))
-    costs.sort()
-    result_dict = unit_economy_calc(margin_item.buy, margin_item.pack, margin_item.commission,
-                                    margin_item.logistic_to_customer, margin_item.storage_price,
-                                    margin_item.returned_percent, margin_item.client_tax, costs,
-                                    margin_item.transit_price, margin_item.transit_count)
-    return result_dict
-
-
-@app.get('/data/{niche}')
-def upload_data(niche: str, is_update: bool = False):
-    text_to_search = niche.lower()
-    text_to_search = re.sub(' +', ' ', text_to_search)
-    load_niche_info(niche, storage_dir, is_update, pages_num=1)
-    filename = abspath(
-        str(join(storage_dir, text_to_search + ".txt"))
-    )
-    cost_data = load_cost_data_from_file(filename)
-    n_samples = int(len(cost_data) * 0.1)  # todo think about number of samples
-    x, y = get_frequency_stats(cost_data, n_samples + 1)
-    return {'x': x, 'y': y}
+@app.get('/save_request/{request}')
+def upload_data(request_json: str):
+    request_to_save: Request = jorm_factory.request(request_json)
+    db_updater.save_request(request_to_save)
 
 
 if __name__ == '__main__':
