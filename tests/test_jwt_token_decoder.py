d311f625447c8f2fb9d28003f785fa989cf4de03
Sawyron <s4pakvv@gmail.com>
2023-01-31 01:34:05 +0700

    TokenDecoder and TokenEncoder interfaces are created and their pyjwt realizations are implemented

---
+++ tests/test_jwt_token_decoder.py
@@ -0,0 +1,23 @@
+import unittest
+from auth.tokens import PyJwtTokenDecoder
+from datetime import datetime
+import jwt
+
+
+class JwtTokenDecoderTest(unittest.TestCase):
+    def test_extract_payload(self):
+        data = {
+            'id': 8,
+            'date': int(datetime(year=2007, month=9, day=3).timestamp())
+        }
+        key = 'secret'
+        algorithm = 'HS256'
+        token = jwt.encode(data, key, algorithm)
+        decoder = PyJwtTokenDecoder(key, [algorithm])
+        payload = decoder.extract_payload(token)
+        for expected_pair, actual_pair in zip(data, payload, strict=True):
+            self.assertEqual(expected_pair, actual_pair)
+
+
+if __name__ == '__main__':
+    unittest.main()
