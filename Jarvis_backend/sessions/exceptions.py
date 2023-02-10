8f66bea83013c0eff78db28bc7de3d42256f2fca
Pelidyai <dima-pile@mail.ru>
2023-02-10 12:26:49 +0700

    Improve JarvisExceptions

--- Jarvis_backend/sessions/exceptions.py
+++ Jarvis_backend/sessions/exceptions.py
@@ -1,27 +1,28 @@
+import json
 from enum import Enum
 
-from fastapi import HTTPException
 from starlette import status
+from starlette.exceptions import HTTPException
 
 
 class JarvisExceptionsCode(Enum):
     # authorization exceptions
-    INCORRECT_LOGIN_OR_PASSWORD: int = 4012
+    INCORRECT_LOGIN_OR_PASSWORD = 4012
 
     # password correctness exceptions
-    LESS_THAN_8: int = 4013
-    NOT_HAS_LOWER_LETTERS: int = 4014
-    NOT_HAS_UPPER_LETTERS: int = 4015
-    NOT_HAS_DIGIT: int = 4016
-    NOT_HAS_SPECIAL_SIGNS: int = 4017
-    HAS_WHITE_SPACES: int = 4018
+    LESS_THAN_8 = 4013
+    NOT_HAS_LOWER_LETTERS = 4014
+    NOT_HAS_UPPER_LETTERS = 4015
+    NOT_HAS_DIGIT = 4016
+    NOT_HAS_SPECIAL_SIGNS = 4017
+    HAS_WHITE_SPACES = 4018
 
     # registration exceptions
-    REGISTER_EXISTING_LOGIN: int = 4019
+    REGISTER_EXISTING_LOGIN = 4019
 
     # token exceptions
-    INCORRECT_TOKEN: int = 5001
-    EXPIRED_TOKEN: int = 5002
+    INCORRECT_TOKEN = 5001
+    EXPIRED_TOKEN = 5002
 
 
 class JarvisExceptions(Enum):
@@ -29,25 +30,35 @@ class JarvisExceptions(Enum):
     def create_exception_with_code(exception_code: int) -> HTTPException:
         return HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=exception_code
+            detail=json.dumps({
+                'jarvis_exception': exception_code
+            })
         )
 
     INCORRECT_TOKEN = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail=JarvisExceptionsCode.INCORRECT_TOKEN,
+        detail=json.dumps({
+            'jarvis_exception': JarvisExceptionsCode.INCORRECT_TOKEN.value
+        })
     )
 
     EXPIRED_TOKEN = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail=JarvisExceptionsCode.EXPIRED_TOKEN
+        detail=json.dumps({
+            'jarvis_exception': JarvisExceptionsCode.EXPIRED_TOKEN.value
+        })
     )
 
     INCORRECT_LOGIN_OR_PASSWORD = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail=JarvisExceptionsCode.INCORRECT_LOGIN_OR_PASSWORD,
+        detail=json.dumps({
+            'jarvis_exception': JarvisExceptionsCode.INCORRECT_LOGIN_OR_PASSWORD.value
+        })
     )
 
     EXISTING_LOGIN = HTTPException(
         status_code=status.HTTP_208_ALREADY_REPORTED,
-        detail=JarvisExceptionsCode.REGISTER_EXISTING_LOGIN
+        detail=json.dumps({
+            'jarvis_exception': JarvisExceptionsCode.REGISTER_EXISTING_LOGIN.value
+        })
     )
