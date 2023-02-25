ffba94ccc75ab064af52e4dad7bfe312b3eb2668
Pelidyai <dima-pile@mail.ru>
2023-02-25 14:39:08 +0700

    Use router prefix in calc requests

--- app/calc/requests.py
+++ app/calc/requests.py
@@ -7,10 +7,10 @@ from app.handlers import session_controller, calculation_controller
 from app.tokens.util import access_token_correctness_depend
 from sessions.request_items import UnitEconomyRequestObject
 
-calc_router = APIRouter()
+calc_router = APIRouter(prefix=ACCESS_TOKEN_USAGE_URL_PART)
 
 
-@calc_router.post(ACCESS_TOKEN_USAGE_URL_PART + '/jorm_margin/')
+@calc_router.post('/jorm_margin/')
 def calc_margin(unit_economy_item: UnitEconomyRequestObject,
                 access_token: str = Depends(access_token_correctness_depend)):
     user: User = session_controller.get_user(access_token)
@@ -25,14 +25,14 @@ def calc_margin(unit_economy_item: UnitEconomyRequestObject,
     return result_dict
 
 
-@calc_router.get(ACCESS_TOKEN_USAGE_URL_PART + '/jorm_data/')
+@calc_router.get('/jorm_data/')
 def upload_data(niche_name: str, _: str = Depends(access_token_correctness_depend)):
     niche: Niche = session_controller.get_niche(niche_name)
     x, y = calculation_controller.calc_frequencies(niche)
     return {'x': x, 'y': y}
 
 
-@calc_router.get(ACCESS_TOKEN_USAGE_URL_PART + '/save_request/')
+@calc_router.get('/save_request/')
 def save_request_to_history(request_json: str,
                             access_token: str = Depends(access_token_correctness_depend)):
     user: User = session_controller.get_user(access_token)
