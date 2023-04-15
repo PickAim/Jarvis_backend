dd40471b0e27b26b1b207b7ce61d1103b1552319
Pelidyai <dima-pile@mail.ru>
2023-04-15 22:55:30 +0700

    Revert changes for token names

--- app/constants.py
+++ app/constants.py
@@ -1,6 +1,6 @@
 ACCESS_TOKEN_USAGE_URL_PART = "/access"
 UPDATE_TOKEN_USAGE_URL_PART = "/update"
 
-ACCESS_TOKEN_NAME = "access-token"
-UPDATE_TOKEN_NAME = "update-token"
-IMPRINT_TOKEN_NAME = "imprint-token"
+ACCESS_TOKEN_NAME = "access_token"
+UPDATE_TOKEN_NAME = "update_token"
+IMPRINT_TOKEN_NAME = "imprint_token"
