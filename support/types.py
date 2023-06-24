a8184b7ae776433e7ee9fe3ba6c0f1e8cdae1f6b
Pelidyai <dima-pile@mail.ru>
2023-06-24 15:42:55 +0700

    Create Jarvis wrappers for items representations

---
+++ support/types.py
@@ -0,0 +1,31 @@
+from jorm.market.service import FrequencyRequest, FrequencyResult, UnitEconomyRequest, UnitEconomyResult, Request, \
+    Result
+
+from sessions.request_items import RequestInfo
+
+
+class JBasicSaveObject:
+    request_type = Request
+    result_type = Result
+    info: RequestInfo
+
+    def __init__(self, request: Request, result: Result, info: RequestInfo):
+        self.request: Request = request
+        self.result: Result = result
+        self.info: RequestInfo = info
+
+
+class JFrequencySaveObject(JBasicSaveObject):
+    request_type = FrequencyRequest
+    result_type = FrequencyResult
+
+    def __init__(self, request: FrequencyRequest, result: FrequencyResult, info: RequestInfo):
+        super().__init__(request, result, info)
+
+
+class JEconomySaveObject(JBasicSaveObject):
+    request_type = UnitEconomyRequest
+    result_type = UnitEconomyResult
+
+    def __init__(self, request: UnitEconomyRequest, result: UnitEconomyResult, info: RequestInfo):
+        super().__init__(request, result, info)
