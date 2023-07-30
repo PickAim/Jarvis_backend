ba79fbd107ad3434da86a0c820580bd4bd0e73e0
Pelidyai <dima-pile@mail.ru>
2023-07-31 00:19:09 +0700

    #9: create get all user products request. Revert removal of default init in tests

--- jarvis_backend/app/info_api.py
+++ jarvis_backend/app/info_api.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends
-from jarvis_factory.factories.jorm import JORMClassesFactory
+from jorm.market.person import User
 
 from jarvis_backend.app.tags import INFO_TAG
+from jarvis_backend.app.tokens.dependencies import access_token_correctness_post_depend
 from jarvis_backend.sessions.controllers import JarvisSessionController
 from jarvis_backend.sessions.dependencies import session_controller_depend
 from jarvis_backend.support.request_api import RequestAPI
@@ -36,23 +37,24 @@ class InfoAPI(RequestAPI):
 
     @staticmethod
     @router.get('/get-all-user-products/')
-    def get_all_user_products():  # TODO add access token and session controller parameter
-        default_niche = JORMClassesFactory.create_default_niche()
-        products = default_niche.products[:10]
+    def get_all_user_products(access_token: str = Depends(access_token_correctness_post_depend),
+                              session_controller: JarvisSessionController = Depends(session_controller_depend)):
+        user: User = session_controller.get_user(access_token)
+        user_products = session_controller.get_products_by_user(user.user_id)
         return {
-            i: {
-                "global_id": products[i].global_id,
-                "name": products[i].name,
-                "category": products[i].category_name,
-                "niche": products[i].niche_name,
-                "cost": products[i].cost,
-                "rating": products[i].rating,
-                "seller": products[i].seller,
-                "brand": products[i].brand,
+            product_id: {
+                "global_id": user_products[product_id].global_id,
+                "name": user_products[product_id].name,
+                "category": user_products[product_id].category_name,
+                "niche": user_products[product_id].niche_name,
+                "cost": user_products[product_id].cost,
+                "rating": user_products[product_id].rating,
+                "seller": user_products[product_id].seller,
+                "brand": user_products[product_id].brand,
                 "history": {
                     history_unit.unit_date.timestamp(): history_unit.cost
-                    for history_unit in products[i].history.get_history()
+                    for history_unit in user_products[product_id].history.get_history()
                 }
             }
-            for i in range(len(products))
+            for product_id in user_products
         }
