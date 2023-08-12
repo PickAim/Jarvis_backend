788da7991d508423ce433f3ea3e5287c3190d0f5
Pelidyai <dima-pile@mail.ru>
2023-08-12 20:12:26 +0700

    Extract constant for cookie-names

--- jarvis_backend/controllers/cookie.py
+++ jarvis_backend/controllers/cookie.py
@@ -3,23 +3,27 @@ from starlette.responses import JSONResponse, Response
 
 from jarvis_backend.app.constants import ACCESS_TOKEN_USAGE_URL_PART, UPDATE_TOKEN_USAGE_URL_PART
 
+COOKIE_ACCESS_NAME = "cookie_access_token"
+COOKIE_UPDATE_NAME = "cookie_update_token"
+COOKIE_IMPRINT_NAME = "cookie_imprint_token"
+
 
 class CookieHandler:
     @staticmethod
     def save_access_token(response: Response, access_token: str) -> Response:
-        response.set_cookie(key="cookie_access_token", path=ACCESS_TOKEN_USAGE_URL_PART,
+        response.set_cookie(key=COOKIE_ACCESS_NAME, path=ACCESS_TOKEN_USAGE_URL_PART,
                             value=access_token, httponly=True, secure=True)
         return response
 
     @staticmethod
     def save_update_token(response: Response, update_token: str) -> Response:
-        response.set_cookie(key="cookie_update_token", value=update_token, httponly=True,
+        response.set_cookie(key=COOKIE_UPDATE_NAME, value=update_token, httponly=True,
                             secure=True)
         return response
 
     @staticmethod
     def save_imprint_token(response: Response, imprint_token: str) -> Response:
-        response.set_cookie(key="cookie_imprint_token", value=imprint_token, httponly=True, secure=True)
+        response.set_cookie(key=COOKIE_IMPRINT_NAME, value=imprint_token, httponly=True, secure=True)
         return response
 
     @staticmethod
@@ -36,6 +40,6 @@ class CookieHandler:
 
     @staticmethod
     def delete_all_cookie(response: JSONResponse) -> JSONResponse:
-        response.delete_cookie("cookie_access_token", path=ACCESS_TOKEN_USAGE_URL_PART, httponly=True, secure=True)
-        response.delete_cookie("cookie_update_token", path=UPDATE_TOKEN_USAGE_URL_PART, httponly=True, secure=True)
+        response.delete_cookie(COOKIE_ACCESS_NAME, path=ACCESS_TOKEN_USAGE_URL_PART, httponly=True, secure=True)
+        response.delete_cookie(COOKIE_UPDATE_NAME, path=UPDATE_TOKEN_USAGE_URL_PART, httponly=True, secure=True)
         return response
