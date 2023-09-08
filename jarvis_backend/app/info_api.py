82a7daf4f8bd9815fed0c4af88185c2104680ff5
Pelidyai <dima-pile@mail.ru>
2023-09-08 22:36:39 +0700

    Adapt info api to new depends

--- jarvis_backend/app/info_api.py
+++ jarvis_backend/app/info_api.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter, Depends
 
 from jarvis_backend.app.tags import INFO_TAG
-from jarvis_backend.controllers.session import JarvisSessionController
-from jarvis_backend.sessions.dependencies import session_controller_depend
+from jarvis_backend.sessions.dependencies import session_controller_depend, session_depend
 from jarvis_backend.sessions.request_items import (GetAllMarketplacesModel,
                                                    GetAllNichesModel,
                                                    GetAllCategoriesModel)
@@ -19,21 +18,21 @@ class InfoAPI(RequestAPI):
     @staticmethod
     @router.post('/get-all-marketplaces/', response_model=dict[int, str])
     def get_all_marketplaces(request_data: GetAllMarketplacesModel = None,
-                             session_controller: JarvisSessionController = Depends(session_controller_depend)) \
-            -> dict[int, str]:
+                             session=Depends(session_depend)) -> dict[int, str]:
+        session_controller = session_controller_depend(session)
         is_allow_defaults = request_data.is_allow_defaults if request_data is not None else False
         return session_controller.get_all_marketplaces(is_allow_defaults)
 
     @staticmethod
     @router.post('/get-all-categories/', response_model=dict[int, str])
     def get_all_categories(request_data: GetAllCategoriesModel,
-                           session_controller: JarvisSessionController = Depends(session_controller_depend)) \
-            -> dict[int, str]:
+                           session=Depends(session_depend)) -> dict[int, str]:
+        session_controller = session_controller_depend(session)
         return session_controller.get_all_categories(request_data.marketplace_id, request_data.is_allow_defaults)
 
     @staticmethod
     @router.post('/get-all-niches/', response_model=dict[int, str])
     def get_all_niches(request_data: GetAllNichesModel,
-                       session_controller: JarvisSessionController = Depends(session_controller_depend)) \
-            -> dict[int, str]:
+                       session=Depends(session_depend)) -> dict[int, str]:
+        session_controller = session_controller_depend(session)
         return session_controller.get_all_niches(request_data.category_id, request_data.is_allow_defaults)
