98c497960eecd96d3c5e671bf32349ad3e9dbe6e
Pelidyai <dima-pile@mail.ru>
2023-09-08 22:32:49 +0700

    Adapt niche analyze api to new depends

--- jarvis_backend/app/calc/niche_analyze_api.py
+++ jarvis_backend/app/calc/niche_analyze_api.py
@@ -9,7 +9,7 @@ from jarvis_backend.app.calc.calculation import CalculationController
 from jarvis_backend.app.calc.calculation_request_api import CalculationRequestAPI
 from jarvis_backend.app.tokens.dependencies import access_token_correctness_post_depend
 from jarvis_backend.controllers.session import JarvisSessionController
-from jarvis_backend.sessions.dependencies import session_controller_depend
+from jarvis_backend.sessions.dependencies import session_controller_depend, session_depend
 from jarvis_backend.sessions.exceptions import JarvisExceptions
 from jarvis_backend.sessions.request_items import NicheCharacteristicsResultModel, NicheRequest, \
     GreenTradeZoneCalculateResultModel
@@ -37,9 +37,10 @@ class NicheCharacteristicsAPI(CalculationRequestAPI):
 
     @staticmethod
     @router.post('/calculate/', response_model=NicheCharacteristicsResultModel)
-    def calculate(request_data: NicheRequest, access_token: str = Depends(access_token_correctness_post_depend),
-                  session_controller: JarvisSessionController = Depends(session_controller_depend)) \
-            -> NicheCharacteristicsResultModel:
+    def calculate(request_data: NicheRequest,
+                  access_token: str = Depends(access_token_correctness_post_depend),
+                  session=Depends(session_depend)) -> NicheCharacteristicsResultModel:
+        session_controller = session_controller_depend(session)
         NicheCharacteristicsAPI.check_and_get_user(session_controller, access_token)
         niche = _check_ang_get_niche(request_data, session_controller)
         result = CalculationController.calc_niche_characteristics(niche)
@@ -63,8 +64,10 @@ class GreenTradeZoneAPI(CalculationRequestAPI):
 
     @staticmethod
     @router.post('/calculate/', response_model=TempResult)
-    def calculate(request_data: NicheRequest, access_token: str = Depends(access_token_correctness_post_depend),
-                  session_controller: JarvisSessionController = Depends(session_controller_depend)):
+    def calculate(request_data: NicheRequest,
+                  access_token: str = Depends(access_token_correctness_post_depend),
+                  session=Depends(session_depend)):
+        session_controller = session_controller_depend(session)
         GreenTradeZoneAPI.check_and_get_user(session_controller, access_token)
         niche = _check_ang_get_niche(request_data, session_controller)
         result = CalculationController.calc_green_zone(niche, datetime.utcnow())
