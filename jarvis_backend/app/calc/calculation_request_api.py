eacab6fc6d57206c3e1aaf3fad4b73d4d2936557
Pelidyai <dima-pile@mail.ru>
2023-09-08 22:42:19 +0700

    Correct savable calculations interface

--- jarvis_backend/app/calc/calculation_request_api.py
+++ jarvis_backend/app/calc/calculation_request_api.py
@@ -5,8 +5,7 @@ from fastapi import Depends, APIRouter
 from jarvis_backend.app.constants import ACCESS_TOKEN_USAGE_URL_PART
 from jarvis_backend.app.tags import CALCULATION_TAG
 from jarvis_backend.app.tokens.dependencies import access_token_correctness_post_depend
-from jarvis_backend.sessions.dependencies import request_handler_depend, session_depend
-from jarvis_backend.sessions.request_handler import RequestHandler
+from jarvis_backend.sessions.dependencies import session_depend
 from jarvis_backend.support.request_api import RequestAPIWithCheck
 
 
@@ -25,20 +24,17 @@ class SavableCalculationRequestAPI(CalculationRequestAPI):
     @staticmethod
     @abstractmethod
     def save(access_token: str = Depends(access_token_correctness_post_depend),
-             session=Depends(session_depend),
-             request_handler: RequestHandler = Depends(request_handler_depend)):
+             session=Depends(session_depend)):
         pass
 
     @staticmethod
     @abstractmethod
     def get_all(access_token: str = Depends(access_token_correctness_post_depend),
-                session=Depends(session_depend),
-                request_handler: RequestHandler = Depends(request_handler_depend)):
+                session=Depends(session_depend)):
         pass
 
     @staticmethod
     @abstractmethod
     def delete(request_id: int,
-               access_token: str = Depends(access_token_correctness_post_depend), session=Depends(session_depend),
-               request_handler: RequestHandler = Depends(request_handler_depend)):
+               access_token: str = Depends(access_token_correctness_post_depend), session=Depends(session_depend)):
         pass
